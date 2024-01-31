7ec207e5f88dc8cf15e3a657d332ad99782e59e1
Alex <monyakrafter@gmail.com>
2024-01-31 21:03:15 +0300

    Title

---
+++ app.js
@@ -0,0 +1,37 @@
+// 1. ------------------------------------------
+
+// let masiv = [2, 4, 7, 10, 3, 9, 1, 6];
+// for (let i = 0; i < masiv.length; i++) {
+//   if (masiv[i] > 3 && masiv[i] < 10) {
+//     console.log(masiv[i]);
+//   }
+// }
+
+// 2. ---------------------------------------
+
+// let masiv2 = [1, 2, 5, 9, 4, 13, 4, 10];
+// for (let i = 0; i < masiv2.length; i++) {
+//   if (masiv2[i] === 4) {
+//     alert("Наявний!");
+//     break;
+//   }
+// }
+
+// 3. ------------------------------------------
+
+// let mass = [42, 2, 33, 11, 12, 10, 0];
+// let allsum = mass.reduce((total, current) => total + current, 0);
+// let sersum = allsum / mass.length;
+// console.log(sersum);
+
+// 4. ---------------------------------------------
+
+// let animals = ["parrot", "bull", "bear", "monkey"];
+// let animalsSum = animals.reduce((total, current) => total + current.length, 0);
+// console.log(animalsSum);
+
+// 5. -------------------------------------------------
+
+// let masiv3 = ["parrot", 140, "bull", true, 0, "bear", 47, "monkey"];
+
+// console.log(masiv3.filter((lol) => typeof lol == "string"));
